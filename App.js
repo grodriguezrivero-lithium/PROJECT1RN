cd6041f96cf172d06a0bde4e58d42eb39633ecd2
Gabriela Rodriguez <grodriguezrivero@lithium.com.uy>
2019-03-14 16:21:19 -0300

    initial commit2

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>First React Native App</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
